37f4b06ccdb651880ed50a4a34b44350c44a23a4
Moin Alam <moinalam010@gmail.com>
2024-03-21 22:02:52 +0600

    +

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-$(document).ready(function () {
-  $(".your-class").slick({
-    //   setting-name: setting-value
-  });
+// $(document).ready(function () {
+//   $(".your-class").slick({
+//     //   setting-name: setting-value
+//   });
+// });
+
+$(".hero-slider-1").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
 });
